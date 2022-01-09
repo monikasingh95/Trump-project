ee5dddbe447f2ff41bbe64378d1e4a2cc8e5bc9b
Monika <monikasingh.singh95@gmail.com>
2022-01-10 00:52:23 +0530

    Fetch function error

---
+++ index.js
@@ -0,0 +1,16 @@
+//import fetch from 'node-fetch';
+
+// const tweets = fetch('https://raw.githubusercontent.com/prasannjeet/Trump-Tweets-Problem/master/data.js')
+//     .then(res => {
+//         if (res.ok) {
+//             console.log('SUCCESS')
+//         }else {
+//             console.log('Not Successful')
+//         }
+//     })
+//     .then(data => console.log(data))
+//     .catch(error => console.log('ERROR'))
+//
+// console.log(tweets);
+
+//const tweets = require('./data.js');
