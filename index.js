356fbdc127ff979945b6d3cfa3c4d8a49edea447
Prasannjeet Singh <Prasannjeet.Singh@ist.com>
2022-01-10 20:17:07 +0100

    Updated index.js to properly load the file. Added some hints on how to use it.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import fetch from 'node-fetch';
 
-const response = await fetch('https://raw.githubusercontent.com/prasannjeet/Trump-Tweets-Problem/master/data.js');
+const response = await fetch('https://raw.githubusercontent.com/monikasingh95/Trump-project/master/data.json');
 const body = await response.text();
 
+const tweets = JSON.parse(body); // This will convert "body" from a string to a JavaScript object
 
+// This will print out all the tweets in the JSON file
+for (let tweet of tweets) {
+  console.log(tweet['text']);
+}
 
+// Thi swill print out the source of all the tweets
+for (let tweet of tweets) {
+  console.log(tweet['source']);
+}
+
+// This is how you can use the data. You can delete above loop and start solving the questions.
