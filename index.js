20644bc31c1f1f4846c5552fac1a1719cf9d81c7
Monika <monikasingh.singh95@gmail.com>
2022-01-10 21:54:07 +0530

    Added fetch function

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-//import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 
-// const tweets = fetch('https://raw.githubusercontent.com/prasannjeet/Trump-Tweets-Problem/master/data.js')
-//     .then(res => {
-//         if (res.ok) {
-//             console.log('SUCCESS')
-//         }else {
-//             console.log('Not Successful')
-//         }
-//     })
-//     .then(data => console.log(data))
-//     .catch(error => console.log('ERROR'))
-//
-// console.log(tweets);
+const response = await fetch('https://raw.githubusercontent.com/prasannjeet/Trump-Tweets-Problem/master/data.js');
+const body = await response.text();
 
-//const tweets = require('./data.js');
+
+
