9a52c9908ee0d3dbc962b3a0a1ad0889581f8fbe
Monika <monikasingh.singh95@gmail.com>
2022-01-11 10:48:22 +0530

    Solved first problem.

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ const body = await response.text();
 const tweets = JSON.parse(body); // This will convert "body" from a string to a JavaScript object
 
 // This will print out all the tweets in the JSON file
-for (let tweet of tweets) {
-  console.log(tweet['text']);
-}
+// for (let tweet of tweets) {
+//   console.log(tweet['text']);
+// }
 
 // Thi swill print out the source of all the tweets
-for (let tweet of tweets) {
-  console.log(tweet['source']);
-}
+// for (let tweet of tweets) {
+//   console.log(tweet['source']);
+//}
 
 // This is how you can use the data. You can delete above loop and start solving the questions.
+
+/*---------------------------------QUESTIONS--------------------------*/
+
+//Q How many tweets are in the data set?
+
+console.log("Number of tweets in the data set are " + tweets.length);
+
