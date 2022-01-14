798b03dea46f6c2745976ae42375e934236158e1
Prasannjeet Singh <Prasannjeet.Singh@ist.com>
2022-01-14 01:54:42 +0100

    Explanation of how to find the tweet with maximum count.

--- index.js
+++ index.js
@@ -42,11 +42,57 @@ var mappingTweet =  tweets.map(favoriteTweet => {
 })
 
 
-var mostLikedTweets = Math.max.apply(Math, mappingTweet);
+// var mostLikedTweets = Math.max.apply(Math, mappingTweet);
 
 // var mostLikedTweets = Math.max.apply(Math, tweets.map(function(favoriteTweet) {
 //         return favoriteTweet.favorite_count; }
 //         ))
 
 
-console.log(mostLikedTweets);
+// console.log(mostLikedTweets);
+
+/* First I will check if Math.max.apply works or not. If it works then I will use it.
+For this I'll first create a number array and then I'll use Math.max.apply to find the maximum number in the array.
+ */
+
+let numberArray = [22, 34, 56, 32, 66, 78, 11, 45, 99];
+let maxNumber = Math.max.apply(Math, numberArray);
+console.log("Maximum Number is: " + maxNumber);
+// This is the output: Maximum Number is: 99 (So the function is correct, and it works).
+
+/* Now I will check what's wrong with your code.
+In your code, I can see that you have applied the same function to the variable "mappingTweet". But, as you can see,
+"mappingTweet" is not a number array. Maximum can only and only be applied to a number array, not to an object array.
+Math.max.apply can only be applied to an array of numbers, just like the variable "numberArray" as above. So what we
+can do is we can try to map tweets to only a number array, and then we can use Math.max.apply to find the maximum number.
+Let's try it.
+ */
+
+let likesCountArray = tweets.map(favoriteTweet => {
+        return favoriteTweet.favorite_count;
+});
+
+/* As you can see in the above code, the variable "likesCountArray" is an array of numbers. It will only be in the
+format like this: [22, 34, 56, 32, 66, 78, 11, 45, 99]. So, we can use Math.max.apply to find the maximum number.
+So let's apply the function now.
+ */
+
+let maxLikesCount = Math.max.apply(Math, likesCountArray);
+console.log("The maximum number of likes is: " + maxLikesCount);
+// This is the output: The maximum number of likes is: 605098 (So the function is correct and it works).
+
+
+//Now let's find the index of this item.
+let index = likesCountArray.indexOf(maxLikesCount); // This will return the index of the maximum number.
+console.log("The index of the maximum number of likes is: " + index); //Answer: 1005
+
+//Now in the original variable "tweets", we will find out the 1005th item.
+let tweet = tweets[index]; // This will return the 1005th item.
+
+//Now let's print out the tweet that got the most likes.
+console.log("The tweet that got the most likes is: " + tweet.text);
+
+/*---------------------------------TWO-LINE-SOLUTION--------------------------*/
+//Everything that we did above can also be done in two lines:
+let likesCountArray2 = tweets.map(t => t.favorite_count);
+console.log("The tweet that got the most likes is: " + tweets[likesCountArray2.indexOf(Math.max.apply(Math, likesCountArray2))].text);
