404b28f514c795385f42a158a42f79c4f54e8543
Monika <monikasingh.singh95@gmail.com>
2022-02-19 20:02:46 +0530

    Chart via javascript

--- probPrtOne.js
+++ probPrtOne.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 const response = await fetch('https://raw.githubusercontent.com/monikasingh95/Trump-project/master/data.json');
-const body = await response.text();
+const body =  await response.text();
 
 const tweets = JSON.parse(body); // This will convert "body" from a string to a JavaScript object
 
@@ -9,23 +9,46 @@ const tweets = JSON.parse(body); // This will convert "body" from a string to a 
 let activeHours = new Map();
 
 for(let tweet of tweets){
-    const newTime = new Date(tweet.created_at);
+    const newTime = new Date(tweet["created_at"]);
     const timeData = newTime.getHours();
-    if(activeHours.has(timeData)){
-        activeHours.set(timeData, activeHours.get(timeData) +1);
+    if(activeHours.has(parseInt(timeData))){
+        activeHours.set(parseInt(timeData), activeHours.get(parseInt(timeData)) +1);
     } else {
-        activeHours.set(timeData, 0);
+        activeHours.set(parseInt(timeData), 0);
     }
 }
-console.log(activeHours);
+
+let mapAsc = Array.from(activeHours.keys()).sort((a,b) => parseInt(a) - parseInt(b));
+
+let sortedMap = new Map();
+mapAsc.forEach(i => sortedMap.set(i, activeHours.get(i)));
+
+//console.log(sortedMap);
+
 
 let mostActiveHour;
 let maxHourCount = 0;
-for(let[key, value] of activeHours) {
+let valueData = [];
+
+for(let [key, value] of sortedMap) {
     if(value > maxHourCount){
         maxHourCount = value;
         mostActiveHour = key;
     }
+    valueData.push(value);
 }
-console.log("Most active Hour: " + mostActiveHour);
-console.log("Number of Tweets: " + maxHourCount);
+console.log(valueData);
+// console.log("Most active Hour: " + mostActiveHour);
+// console.log("Number of Tweets: " + maxHourCount);
+
+//return valueData
+
+
+
+
+
+
+
+
+
+
